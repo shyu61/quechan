c92dd11c0f59a672b5678c273590543aff568d5a
shyu61 <ohs6261@gmail.com>
2021-09-23 23:42:43 +0900

    Create pub/sub

--- main.go
+++ main.go
@@ -1,58 +1,83 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
-	"sync"
 )
 
+type PublishRequest struct {
+	Topic   string `json:"topic"`
+	Message string `json:"message"`
+}
+
+type SubscribeRequest struct {
+	Topic string `json:"topic"`
+}
+
+var queue = make(map[string][]string)
+
 // POST /publish
-func publisher(w http.ResponseWriter, r *http.Request, m sync.Map) {
+func publisher(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		fmt.Fprint(w, "Invalid http method")
 		return
 	}
-	m.Store("name", r.Body)
-	m.Range(func(key, value interface{}) bool {
-		fmt.Fprintf(w, "%v: %v", key, value)
-		return true
-	})
-	fmt.Fprintf(w, "OK!!")
+
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var req PublishRequest
+	if err := json.Unmarshal(body, &req); err != nil {
+		log.Fatal(err)
+	}
+
+	queue[req.Topic] = append(queue[req.Topic], req.Message)
+	fmt.Fprintf(w, "Enqued: %v", queue)
 }
 
-func subscriber(w http.ResponseWriter, r *http.Request, m sync.Map) {
+// POST /subscribe
+func subscriber(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		fmt.Fprint(w, "Invalid http method")
 		return
 	}
-	m.Range(func(key, value interface{}) bool {
-		fmt.Printf("%v: %v", key, value)
-		return true
-	})
-	fmt.Fprintf(w, "OK!!!!!")
+
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var req SubscribeRequest
+	if err := json.Unmarshal(body, &req); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprintf(w, "Dequed: %s", queue[req.Topic][0])
+	queue[req.Topic] = queue[req.Topic][1:]
 }
 
 func main() {
-	m := sync.Map{}
-
-	http.HandleFunc("/publish", func(w http.ResponseWriter, r *http.Request) {
-		publisher(w, r, m)
-	})
-	http.HandleFunc("/subscribe", func(w http.ResponseWriter, r *http.Request) {
-		subscriber(w, r, m)
-	})
+	http.HandleFunc("/publish", publisher)
+	http.HandleFunc("/subscribe", subscriber)
 
 	http.ListenAndServe(":8080", nil)
 }
 
+// publish形式: { topic: string, message: string }
+// {"topic": "sample", "message": '{"name": "yamada ichiro", "age": "20"}'}
+
+// subscribe形式: { topic: string }
+// {"topic": "sample"}
+
 // キューイング機能の実装
-	// 0. HTTPリクエストを処理できるようにする。echoサーバー入れるのが良いかな。
-	// 1. キューを格納する構造体の配列を用意する。
-	// 2. publishするエンドポイントと、subscribeするエンドポイントを用意する。
-	// 	- HTTPリクエストでPOSTされていくるオブジェクトをJSONにシリアライズする。
-	// 	- FIFOで実装する
-	// 	- publishするときにtopic名を受け取るようにし、publish/subscribeに名前をつけて識別する。topic名は一意制約をかける。
-	// 3. キューがいっぱいの時、空の時のpub/subのそれぞれの挙動を制御する。
-	// 4. リングバッファを考慮する。
+// 	- publishするときにtopic名を受け取るようにし、publish/subscribeに名前をつけて識別する。topic名は一意制約をかける。
+// 3. キューがいっぱいの時、空の時のpub/subのそれぞれの挙動を制御する。
