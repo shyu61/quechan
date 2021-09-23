9aad24e3e8ba8007faa780bbe9360a9f44c08eca
shyu61 <shu.omura@zenkigen.jp>
2021-09-23 18:23:32 +0900

    Create publisher and subscriber

--- main.go
+++ main.go
@@ -2,10 +2,49 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"sync"
 )
 
+// POST /publish
+func publisher(w http.ResponseWriter, r *http.Request, m sync.Map) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprint(w, "Invalid http method")
+		return
+	}
+	m.Store("name", r.Body)
+	m.Range(func(key, value interface{}) bool {
+		fmt.Fprintf(w, "%v: %v", key, value)
+		return true
+	})
+	fmt.Fprintf(w, "OK!!")
+}
+
+func subscriber(w http.ResponseWriter, r *http.Request, m sync.Map) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprint(w, "Invalid http method")
+		return
+	}
+	m.Range(func(key, value interface{}) bool {
+		fmt.Printf("%v: %v", key, value)
+		return true
+	})
+	fmt.Fprintf(w, "OK!!!!!")
+}
+
 func main() {
-	fmt.Println("Hello, world!")
+	m := sync.Map{}
+
+	http.HandleFunc("/publish", func(w http.ResponseWriter, r *http.Request) {
+		publisher(w, r, m)
+	})
+	http.HandleFunc("/subscribe", func(w http.ResponseWriter, r *http.Request) {
+		subscriber(w, r, m)
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
 
 // キューイング機能の実装
